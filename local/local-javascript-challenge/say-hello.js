5ad0580b609b749f7c01fce179c028d6b9ab0ec7
paulchristianlabor <paulchristianlaborofficial@gmail.com>
2023-06-14 19:13:47 +0800

    updates

---
+++ local/local-javascript-challenge/say-hello.js
@@ -0,0 +1,13 @@
+/**
+ * make a function names sayHello, which accepts a number n
+ * and logs the word "hello" n amount of times.
+ */
+
+function sayHello (n = 5){
+    console.log("hello")
+    console.log("hello")
+    console.log("hello")
+    console.log("hello")
+    console.log("hello")
+}
+sayHello()
