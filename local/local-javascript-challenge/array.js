38a9ad703f14bd97996f0825095eb635242fec11
paulchristianlabor <paulchristianlaborofficial@gmail.com>
2023-07-01 23:39:30 +0800

    updates

--- local/local-javascript-challenge/array.js
+++ local/local-javascript-challenge/array.js
@@ -6,7 +6,11 @@ Display array in a list in HTML just using javascript
 
 /* My Code Starts Here */
 
-const array = (one , two , three , four , five)
+const array = ["one" , "two" , "three" , "four" , "five"]
 let li = document.createElement("li")
-
-li.appendChild(array)
+let number = ""
+for (let i = 0; i<array.length; i++){
+    number += array[i]
+}
+li.appendChild(number)
+number.innerHTML = array
