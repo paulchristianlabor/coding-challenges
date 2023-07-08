d4ffbbb38c798a18a805e11b66dfff9bd655cb49
paulchristianlabor <paulchristianlaborofficial@gmail.com>
2023-07-08 22:32:27 +0800

    array

--- local/local-javascript-challenge/array-3.js
+++ local/local-javascript-challenge/array-3.js
@@ -1,8 +1,15 @@
 /*
+Declare an array variable with any number of any items.
 
-Use i to get the item in array
+Declare a variable i and assign a number value that is less than the array length.
+
+Use i to get an item in the array.
 
 */
 
 /* My Code Starts Here */
 
+const array = [1,2,3,4,5]
+const i = 1
+
+console.log(array[i])
