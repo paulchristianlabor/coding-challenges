0cb0a77ff3c2ec72c2a6c63678b2a127ade993fb
paulchristianlabor <paulchristianlaborofficial@gmail.com>
2023-07-08 22:40:14 +0800

    array

---
+++ local/local-javascript-challenge/array-4.js
@@ -0,0 +1,12 @@
+/*
+
+Write a simple Javascript program to join all elements of the following array into a string.
+Sample array: const myColors = ["Red", "Green", "White", "Black"]
+
+Expected output:
+"Red,Green,White,Black"
+
+*/
+
+/* My Code Starts Here */
+
