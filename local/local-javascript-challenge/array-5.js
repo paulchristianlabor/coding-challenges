f5dd885d6d6de9ae94bebaacdf6afe103b275a5b
paulchristianlabor <paulchristianlaborofficial@gmail.com>
2023-07-09 23:18:16 +0800

    array

--- local/local-javascript-challenge/array-5.js
+++ local/local-javascript-challenge/array-5.js
@@ -6,3 +6,23 @@ Write a JavaScript function that checks whether an argument is an array or not.
 
 /* My Code Starts Here */
 
+// program to check if an object is an array
+
+function checkObject(arr) {
+
+    // check if arr is array
+    const result = Array.isArray(arr);
+
+    if(result) {
+        console.log(`[${arr}] is an array.`);
+    }
+    else {
+        console.log(`${arr} is not an array.`);
+    }
+
+}
+
+const array = [1, 2, 3];
+
+// call the function
+checkObject(1);
